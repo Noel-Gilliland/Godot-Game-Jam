4e0f37ff88942c9b23d61fdf2bd76d92ca979f9f
Noel Gilliland <noelgilliland03@gmail.com>
2025-05-01 13:15:28 -0700

    added background and char

--- main_menu.gd
+++ main_menu.gd
@@ -2,11 +2,11 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://node_2d.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://character_select.tscn") # Replace with function body.
 
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://setting.tscn")
 
 
 func _on_quit_pressed() -> void:
