1c08f0fbaa979fa879820428dd7a606006026092
Noel Gilliland <noelgilliland03@gmail.com>
2025-05-01 14:57:26 -0700

    new scenes

--- main_menu.gd
+++ main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://character_select.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://first_battle.tscn") # Replace with function body.
 
 
 func _on_settings_pressed() -> void:
